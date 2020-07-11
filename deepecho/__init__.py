bfc1fb87e8c6bc4b1772c085c78e43d1a0857157
Kevin Alex Zhang <kevz@mit.edu>
2020-07-11 18:01:19 -0400

    Bump version: 0.0.1 → 0.0.2.dev0

--- deepecho/__init__.py
+++ deepecho/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Kevin Alex Zhang'
 __email__ = 'kevz@mit.edu'
-__version__ = '0.0.1'
+__version__ = '0.0.2.dev0'
 
 from .base import DeepEcho
 from .par import PARModel
--- setup.py
+++ setup.py
@@ -90,6 +90,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/DeepEcho',
-    version='0.0.1',
+    version='0.0.2.dev0',
     zip_safe=False,
 )
