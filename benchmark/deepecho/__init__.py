c69af8fe55f174f2320e6e304b10141c944bf1fe
Carles Sala <carles@pythiac.com>
2020-07-15 20:07:43 +0200

    Fix installation of benchmark

--- benchmark/deepecho/__init__.py
+++ benchmark/deepecho/__init__.py
@@ -1,3 +1,11 @@
-"""DeepEcho benchmarking setup. Nothin other than the __path__ line should be included here."""
+"""DeepEcho benchmarking setup. This file must be identical to deepecho/__init__.py."""
 
+__author__ = 'MIT Data To AI Lab'
+__email__ = 'dailabmit@gmail.com'
+__version__ = '0.0.2.dev0'
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+from deepecho.base import DeepEcho
+from deepecho.par import PARModel
+
+__all__ = ['DeepEcho', 'PARModel']
--- benchmark/setup.py
+++ benchmark/setup.py
@@ -10,52 +10,16 @@ with open('README.md', encoding='utf-8') as readme_file:
 
 install_requires = [
     'boto3>=1,<2',
-    'sktime>=0.4,<0.5',
+    'sktime>=0.3,<0.4',
     'sdmetrics>=0.0.2.dev0',
-    'scikit-learn<0.23,>=0.21',
+    'scikit-learn>=0.21,<0.23',
     'docutils<0.15,>=0.10',
-    'matplotlib<3.2.2,>=2.2.2',
-    'scipy<1.3,>=1.2'
+    'matplotlib>=2.2.2,<3.2.2',
+    'scipy>=1.2,<1.3'
 ]
 
 setup_requires = [
     'pytest-runner>=2.11.1',
-]
-
-tests_require = [
-    'pytest>=3.4.2',
-    'pytest-cov>=2.6.0',
-]
-
-development_requires = [
-    # general
-    'bumpversion>=0.5.3',
-    'pip>=9.0.1',
-    'watchdog>=0.8.3',
-
-    # docs
-    'm2r>=0.2.0,<0.3',
-    'nbsphinx>=0.5.0,<0.7',
-    'Sphinx>=1.7.1,<3',
-    'sphinx_rtd_theme>=0.2.4,<0.5',
-    'autodocsumm>=0.1.10',
-    'jupyterlab>=2.1.0',
-
-    # style check
-    'flake8>=3.7.7',
-    'isort>=4.3.4',
-
-    # fix style issues
-    'autoflake>=1.2',
-    'autopep8>=1.4.3',
-
-    # distribute on PyPI
-    'twine>=1.10.0',
-    'wheel>=0.30.0',
-
-    # Advanced testing
-    'coverage>=4.5.1',
-    'tox>=2.9.1',
 ]
 
 setup(
@@ -67,19 +31,15 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
     ],
-    description='Mixed-type multivariate time series modeling with generative adversarial networks.',
+    description='Benchmarking for mixed-type multivariate time series modeling tools.',
     entry_points = {
         'console_scripts': [
             'deepecho-benchmark=deepecho.benchmark.__main__:main'
         ],
-    },
-    extras_require={
-        'test': tests_require,
-        'dev': development_requires + tests_require,
     },
     install_package_data=True,
     install_requires=install_requires,
@@ -87,14 +47,13 @@ setup(
     long_description=readme,
     long_description_content_type='text/markdown',
     include_package_data=True,
-    keywords='deepecho deepecho DeepEcho',
-    name='deepecho_benchmark',
+    keywords='deepecho benchmark',
+    name='deepecho-benchmark',
     packages=find_packages(include=['deepecho.benchmark.*']),
-    python_requires='>=3.6',
+    python_requires='>=3.5',
     setup_requires=setup_requires,
     test_suite='tests',
-    tests_require=tests_require,
     url='https://github.com/sdv-dev/DeepEcho',
-    version='0.0.1.dev0',
+    version='0.0.2.dev0',
     zip_safe=False,
 )
