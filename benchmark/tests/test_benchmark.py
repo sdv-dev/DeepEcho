43e81e60f32f773579eb91897d6d8c89a5dac65c
Carles Sala <carles@pythiac.com>
2020-08-11 12:48:31 +0200

    Fix tests

--- benchmark/tests/test_benchmark.py
+++ benchmark/tests/test_benchmark.py
@@ -5,14 +5,14 @@ from deepecho.benchmark.dataset import Dataset
 def test_run_benchmark():
     models = [('PARModel', {'epochs': 5, 'cuda': False, 'verbose': False})]
     datasets = [Dataset('Libras', max_entities=10)]
-    metrics = ['sdmetrics', 'detection']
+    metrics = ['sdmetrics', 'rf_detection']
     results = run_benchmark(
         models=models,
         datasets=datasets,
         metrics=metrics
     )
 
-    expected = ['model', 'dataset', 'fit_time', 'sample_time', 'detection_lstm',
-                'detection_rf', 'detection_time', 'sdmetrics', 'sdmetrics_time']
+    expected = ['model', 'dataset', 'fit_time', 'sample_time', 'rf_detection',
+                'rf_detection_time', 'sdmetrics', 'sdmetrics_time']
     assert list(results.columns) == expected
-    assert results.shape == (1, 9)
+    assert results.shape == (1, 8)
