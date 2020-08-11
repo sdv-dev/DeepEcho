6422a1903d7117a0358acedca607660d5af5214b
Carles Sala <carles@pythiac.com>
2020-08-11 22:26:23 +0200

    Bump version: 0.1.0.dev0 → 0.1.0.dev1

--- deepecho/__init__.py
+++ deepecho/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'MIT Data To AI Lab'
 __email__ = 'dailabmit@gmail.com'
-__version__ = '0.1.0.dev0'
+__version__ = '0.1.0.dev1'
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 from deepecho.base import DeepEcho
