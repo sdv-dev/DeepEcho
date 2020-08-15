4f68608ae4eeb3787775ee9d36f07a57a53d36aa
Carles Sala <carles@pythiac.com>
2020-08-15 20:46:34 +0200

    Bump version: 0.1.1 → 0.1.2.dev0

--- benchmark/deepecho/__init__.py
+++ benchmark/deepecho/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'MIT Data To AI Lab'
 __email__ = 'dailabmit@gmail.com'
-__version__ = '0.1.1'
+__version__ = '0.1.2.dev0'
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 from deepecho.base import DeepEcho
--- benchmark/setup.py
+++ benchmark/setup.py
@@ -3,7 +3,7 @@
 
 """The setup script."""
 
-__version__ = '0.1.1'
+__version__ = '0.1.2.dev0'
 
 from setuptools import setup, find_packages
 
