cbbe78f3edde25879ce3aceac07432f0affa2bc1
Carles Sala <carles@pythiac.com>
2020-08-29 16:05:12 +0200

    Add BasicGAN to init

--- deepecho/__init__.py
+++ deepecho/__init__.py
@@ -6,9 +6,11 @@ __version__ = '0.1.2.dev0'
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 from deepecho.demo import load_demo
+from deepecho.models.basic_gan import BasicGANModel
 from deepecho.models.par import PARModel
 
 __all__ = [
     'load_demo',
+    'BasicGANModel',
     'PARModel',
 ]
