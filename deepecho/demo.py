075488e5d21883e1a2a5456cc051f69c004d8647
Carles Sala <carles@pythiac.com>
2020-12-18 10:59:52 +0100

    Parse demo dates

--- deepecho/demo.py
+++ deepecho/demo.py
@@ -9,4 +9,4 @@ _DATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
 
 def load_demo():
     """Load the demo DataFrame."""
-    return pd.read_csv(os.path.join(_DATA_PATH, 'demo.csv'))
+    return pd.read_csv(os.path.join(_DATA_PATH, 'demo.csv'), parse_dates=['date'])
