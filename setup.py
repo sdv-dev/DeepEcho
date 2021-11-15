8bb537cd75bc36ee5dc86f53cfcbac8259bb880c
Felipe Alex Hofmann <fealho@gmail.com>
2021-11-15 05:33:28 -0800

    Bumpy torch version (#45)

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_requires = [
     "numpy>=1.18.0,<1.20.0;python_version<'3.7'",
     "numpy>=1.20.0,<2;python_version>='3.7'",
     'pandas>=1.1.3,<2',
-    'torch>=1.7.1,<2',
+    'torch>=1.8.0,<2',
     'tqdm>=4.15,<5',
 ]
 
