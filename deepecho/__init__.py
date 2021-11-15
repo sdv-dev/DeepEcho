b019cea7ccebd64dd04d40707786f14888532fdf
Felipe Hofmann <fealho@gmail.com>
2021-11-15 06:26:59 -0800

    Bump version: 0.3.0.dev1 → 0.3.0

--- deepecho/__init__.py
+++ deepecho/__init__.py
@@ -5,7 +5,7 @@ __email__ = 'dailabmit@gmail.com'
 <<<<<<< HEAD
 __version__ = '0.2.1'
 =======
-__version__ = '0.3.0.dev1'
+__version__ = '0.3.0'
 >>>>>>> master
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
