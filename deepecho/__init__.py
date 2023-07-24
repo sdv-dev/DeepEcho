59617ab6c7366eef38f21f7751c32fd686437160
Andrew Montanez <andrew@sdv.dev>
2023-07-24 14:41:56 -0500

    Bump version: 0.4.2.dev0 → 0.4.2.dev1

--- deepecho/__init__.py
+++ deepecho/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'DataCebo, Inc.'
 __email__ = 'info@sdv.dev'
-__version__ = '0.4.2.dev0'
+__version__ = '0.4.2.dev1'
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 from deepecho.demo import load_demo
--- setup.py
+++ setup.py
@@ -112,6 +112,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/DeepEcho',
-    version='0.4.2.dev0',
+    version='0.4.2.dev1',
     zip_safe=False,
 )
