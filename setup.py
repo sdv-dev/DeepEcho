04d12d97fab19e2277cf699443ca2637e87ce4ab
Andrew Montanez <andrew@sdv.dev>
2023-07-25 12:38:54 -0500

    Bump version: 0.4.2.dev1 → 0.4.2

--- setup.py
+++ setup.py
@@ -112,6 +112,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/DeepEcho',
-    version='0.4.2.dev1',
+    version='0.4.2',
     zip_safe=False,
 )
