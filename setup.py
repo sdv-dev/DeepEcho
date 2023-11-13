04fa109bdfaabd34b1f6bb1f789ee883d8ac5e6d
lajohn4747 <john@datacebo.com>
2023-11-13 12:24:40 -0600

    Bump version: 0.5.0.dev1 → 0.5.0

--- setup.py
+++ setup.py
@@ -112,6 +112,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/DeepEcho',
-    version='0.5.0.dev1',
+    version='0.5.0',
     zip_safe=False,
 )
