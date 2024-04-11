1621ab921d219eee11e55af6811992afcc7e41a2
Andrew Montanez <andrew@sdv.dev>
2024-04-10 21:18:22 -0500

    Bump version: 0.6.0.dev1 → 0.6.0

--- deepecho/__init__.py
+++ deepecho/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'DataCebo, Inc.'
 __email__ = 'info@sdv.dev'
-__version__ = '0.6.0.dev1'
+__version__ = '0.6.0'
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 from deepecho.demo import load_demo
